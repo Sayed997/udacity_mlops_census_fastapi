7d58b7e59d86536a6be03c462dc3e1b5811cb005
student <studen@example.com>
2026-06-22 19:44:01 +0000

    First version of train_model.py completed

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,19 @@
 # Script to train machine learning model.
 
-from sklearn.model_selection import train_test_split
-
 # Add the necessary imports for the starter code.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle
+
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
+# Load cleaned census data
+data = pd.read_csv("../data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 cat_features = [
     "workclass",
@@ -24,5 +30,32 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
+
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+
+# Predict
+preds = inference(model, X_test)
+
+# Evaluation metrics
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+
+
+
+with open("../model/model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+with open("../model/encoder.pkl", "wb") as f:
+    pickle.dump(encoder, f)
+
+with open("../model/lb.pkl", "wb") as f:
+    pickle.dump(lb, f)
