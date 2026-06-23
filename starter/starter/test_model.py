b74dac9126b144a75320a670c1c6298e6e7cd65f
student <student@udacity.com>
2026-06-23 14:50:23 +0000

    Add function unit test and docstrings

---
+++ starter/starter/test_model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from starter.ml.data import process_data
+from starter.ml.model import train_model, inference, compute_model_metrics
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+def test_process_data_training():
+    #Test process_data function outputs
+    df = pd.read_csv("../data/census.csv").sample(100)
+    X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
+    assert X.shape[0] == y.shape[0]
+    assert encoder is not None
+    assert lb is not None
+
+def test_train_model():
+    #Test train_model function outputs
+    df = pd.read_csv("../data/census.csv").sample(100)
+    X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
+    model = train_model(X, y)
+    assert hasattr(model, "predict")
+
+#Using training set for testing just to test model functionality here
+def test_inference():
+    #Test inference function model outputs
+    df = pd.read_csv("../data/census.csv").sample(100)
+    X, y, encoder, lb = process_data(df, categorical_features=cat_features, label="salary", training=True)
+    model = train_model(X, y)
+    preds = inference(model, X)
+    assert len(preds) == len(y)
+
+def test_compute_model_metrics():
+    #Test compute model metrics function outputs with dummy data
+    y = np.array([0, 1, 1, 0])
+    preds = np.array([0, 1, 0, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -13,6 +13,7 @@ from ml.model import train_model, compute_model_metrics, inference
 data = pd.read_csv("../data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Data splitting
 train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 cat_features = [
@@ -25,11 +26,13 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+#process data with pipeline
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
 X_test, y_test, _, _ = process_data(
     test,
     categorical_features=cat_features,
